11094376ef7806caecc4321bd0b46b7f57a8cbd9
Niranjan <niranjan.prithviraj@gmail.com>
2012-08-09 14:06:29 +0530

    Added conditions and got basic plugin working

--- src/gallery-datatable-selectivecheckbox/js/datatable-selectivecheckbox.js
+++ src/gallery-datatable-selectivecheckbox/js/datatable-selectivecheckbox.js
@@ -1,33 +1,66 @@
+function _dtSelectiveCheckbox(config) {
+	_dtSelectiveCheckbox.superclass.constructor.apply(this, arguments);
+}
 /*
-* This wrapper code is added by the build system.
-* It's only here for informational purposes.
-*/
-YUI.add('gallery-datatable-selectivecheckbox', function(Y) {
-	Y.namespace('DataTableSelectiveCheckbox');
-	function DataTableSelectiveCheckbox(config) {
-		DataTableSelectiveCheckbox.superclass.constructor.apply(this, arguments);
-	}
-	Y.mix(DataTableSelectiveCheckbox,{
-		NAME : 'dataTableSelectiveCheckbox',
-		NS : 'selectivecheckbox',
-		ATTRS : {
-			lColumn : '',
-			rColumn : '',
+	Formatter function for the checkbox column
+	{   key:        'dtSelectiveCheckbox',
+		allowHTML:  true, // to avoid HTML escaping
+		label:      '<input type="checkbox" class="protocol-select-all" title="Toggle ALL records"/>',
+		formatter:      dtSelectiveCheckboxFormatter,
+		dtSelectiveCheckbox:{
+			lColumn : 'pname',
+			rData : 'FTP',
 			condition : 'eq'
 		}
-	});
-	DataTableSelectiveCheckbox =  {
-			init : function(o){
-				if(o.column.dtSelectiveCheckbox){
-					var lColumn = o.column.dtSelectiveCheckbox.lColumn;
-					console.log(o.column.dtSelectiveCheckbox);
-					console.log(o.data[lColumn]);
+	},
+*/
+function dtSelectiveCheckboxFormatter(o){
+	if(o.column.dtSelectiveCheckbox){
+		var scConfig = o.column.dtSelectiveCheckbox;
+		var scCheckboxValue = '<input type="checkbox" class="protocol-select" title="Toggle ALL records"/>';
+		var rCondition,lCondition;
+		if(scConfig.lColumn){
+			lCondition = o.data[scConfig.lColumn];
+		}else if(scConfig.lData){
+			lCondition = scConfig.lData;
+		}
+		if(scConfig.rColumn){
+			rCondition = o.data[scConfig.rColumn];
+		}else if(scConfig.rData){
+			rCondition = scConfig.rData;
+		}
+		switch (scConfig.condition){
+			case "gt":
+				if(rCondition > lCondition){
+					o.value = scCheckboxValue;
 				}
-			},
-			selectAllCheckBox : function(){
-			},
-			getSelected : function(){
-			}
-	};
-	Y.DataTableSelectiveCheckbox = DataTableSelectiveCheckbox;
-},'1.0.0',{requires:['datatable','node','event']});
+			break;
+			case "lt":
+				if(rCondition < lCondition){
+					o.value = scCheckboxValue;
+				}
+			break;
+			case "eq":
+				if(rCondition == lCondition){
+					o.value = scCheckboxValue;
+				}
+			break;
+		}
+	}
+}
+Y.mix(_dtSelectiveCheckbox, {
+	NS: "selectivecheckbox",
+	NAME: "dataTableSelectiveCheckbox",
+});
+Y.extend(_dtSelectiveCheckbox,Y.Plugin.Base,{
+	initializer: function(config){
+		var dt = config.host;
+	},
+	selectAllCheckBox : function(){
+	},
+	getSelected : function(){
+	}
+});
+Y.namespace("Plugin");
+Y.Plugin.DataTableSelectiveCheckbox = _dtSelectiveCheckbox;
+
