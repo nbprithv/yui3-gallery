0c6904982c2fad16df948550b89c21421ecbd9dd
Niranjan <niranjan.prithviraj@gmail.com>
2013-02-22 11:22:29 +0530

    Added some configurations to position the cards more precisely

--- src/gallery-introtour-ui/js/gallery-introtour-ui.js
+++ src/gallery-introtour-ui/js/gallery-introtour-ui.js
@@ -5,8 +5,8 @@ Set up step by step intro tours on your page.
 Usage: 
 	YUI().use("gallery-introtour-ui",function(Y){
 		var tour_cards = [{'title':'Welcome','position':'pagecenter','content':},
-							{'title':'Click here to start','divfocus':'#clickhere','position':'right'},
-							{'title':'Next click here to cont..','divfocus':'#continue','position':'right'},
+							{'title':'Click here to start','divfocus':'#clickhere','position':'right','width':'30'},
+							{'title':'Next click here to cont..','divfocus':'#continue','position':'top',height:'20'},
 							{'title':'That's it!','position':'pagecenter'}];
 		//OPTIONAL: You can style your card's button colors, card border color, title color, button text color, content color.
 		//These are the default values
@@ -34,16 +34,44 @@ YUI.add('gallery-introtour-ui', function(Y) {
 			}
 		return [curleft,curtop];
 	};
-function getOffset( el ) {
-    var _x = 0;
-    var _y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
+	function findpos1( _el ) {
+		var    
+		   target = _el, 
+		   target_width = target.offsetWidth, 
+		   target_height = target.offsetHeight,
+		   target_left = target.offsetLeft,
+		   target_top = target.offsetTop,
+		   gleft = 0, 
+		   gtop = 0,
+		   rect = {};
+		  
+		var moonwalk = function( _parent ) {
+			if (!!_parent) {
+				gleft += _parent.offsetLeft;
+				gtop += _parent.offsetTop;
+				moonwalk( _parent.offsetParent );
+			} else {
+				return rect = { 
+					 top: target.offsetTop + gtop, 
+					 left: target.offsetLeft + gleft, 
+					 bottom: (target.offsetTop + gtop) + target_height, 
+					 right: (target.offsetLeft + gleft) + target_width 
+				};
+			}
+		};
+		moonwalk( target.offsetParent );
+		return rect;
+	}
+	function getOffset( el ) {
+		var _x = 0;
+		var _y = 0;
+		while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+			_x += el.offsetLeft - el.scrollLeft;
+			_y += el.offsetTop - el.scrollTop;
+			el = el.offsetParent;
+		}
+		return { top: _y, left: _x };
+	}
 	var ATTRS = {
 		'cardstyle':{'button':'#61399d','buttontext':'#000','title':'#fff','content':'#fff','cardborder':'#61399d'},
 		'carddimension':{'height':'300px','width':'100px'},
@@ -62,22 +90,67 @@ function getOffset( el ) {
 		}
 		return cardstyle;
 	};
-	var generateSlideDom = function(toppos,leftpos,offsetParent){
-		var html = "<div style='position:fixed;width:100px;height:300px;background-color:#000;color:#FFF;border:color:#000;top:"+toppos+";left:"+leftpos+"'></div>";
+	var generateSlideDom = function(toppos,leftpos,content,title,id){
+		var html = "<div>"+title+"</div>";
+		var node = new Y.Node(document.createElement('div'));
+		toppos = toppos+"px";
+		leftpos = leftpos+"px";
+		node.addClass('galleryintrotourui-card');
+		node.setAttribute('id',id);
+		node.setStyle('top',toppos);
+		node.setStyle('left',leftpos);
+		node.set('innerHTML',html);
+		Y.one('body').appendChild(node);
+	};
+	var getCardPos = function(ci,pos){
+		var toppos = 0;
+		var leftpos = 0;
+		if((ci.position == 'left' || ci.position == 'right') && ci.width == 'undefined')
+			ci.width = 0;
+		if((ci.position == 'top' || ci.position == 'bottom') && ci.height == 'undefined')
+			ci.width = 0;
+		pos[0] = parseInt(pos[0]);
+		pos[1] = parseInt(pos[1]);
+		ci.width = parseInt(ci.width);
+		switch(ci.position){
+			case "right":
+				toppos = pos[1];
+				leftpos = pos[0]+ci.width;
+			break;
+			case "left":
+				toppos = pos[1];
+				leftpos = pos[0]-ci.width;
+			break;
+			case "top":
+				toppos = pos[1]-ci.height;
+				leftpos = pos[0];
+			break;
+			case "bottom":
+				toppos = pos[1]+ci.height;
+				leftpos = pos[0];
+			break;
+			default:
+				toppos = pos[1];
+				leftpos = pos[0]+50;
+			break;
+		};
+		pos[1] = toppos;
+		pos[0] = leftpos;
+		return pos;	
 	};
 	Y.Introtour.init = function(cardinfo,cardstyle){
 		cardstyle = setcardstyle(cardstyle);
-		for(var i=0;i<cardinfo.length;i++){
+		for(var i=1;i<cardinfo.length;i++){
 			var ci = cardinfo[i];
 			var elem = document.getElementById(ci.divfocus);
 			if(elem){
 				var pos = findpos(elem);
-				console.log(pos);
-				var slide = generateSlideDom(toppos,leftpos,content,title);
+				pos = getCardPos(ci,pos);
+				var title = ci.title;
+				var content = '';
+				var id='galleryintrotourui-card-'+i;
+				var slide = generateSlideDom(pos[1],pos[0],content,title,id);
 			}
 		}
-			console.log(document.getElementById("test").offsetParent);
-			console.log(findpos(document.getElementById("test")));
-			console.log(getOffset(document.getElementById("test")));
 	};
-});
+}, '0.1.1',{requires: ['node']});
